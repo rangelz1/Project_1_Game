4cf078ac8ed5a8bb89dd9a84b23e1695f5d28ada
rangelz1 <rangelz187@gmail.com>
2017-04-07 11:43:24 -0700

    Friday

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 var game = {
   round: 0,
@@ -13,34 +12,10 @@ var game = {
   },
   
 }
-=======
-$(document).ready(function() {
- console.log($)
-var sequence = [];
-var player = [];
- var sounds = [
-    "sounds/1.mp3", 
-    "sounds/2.mp3", 
-    "sounds/3.mp3", 
-    "sounds/4.mp3"
-  ];
- var red = "red";
-var blue = "blue";
-var yellow = "yellow";
-var green = "green";
-var colors = [red, blue, yellow, green];
-var color;
-var redSound = new Audio( 'sounds/1.mp3');
-var blueSound = new Audio( 'sounds/2.mp3');
-var yellowSound = new Audio( 'sounds/3.mp3');
-var greenSound = new Audio( 'sounds/4.mp3');
-var errorSound = new Audio("http://www.freesfx.co.uk/rx2/mp3s/10/12671_1442589611.mp3");
-var sound;
->>>>>>> c8768ec61d18a0e35bcdeceaf334129c9de8fce6
 
 function clearGame() {
-  game.currentGame = [];
-  game.count = 0;
+  game.sequence = [];
+  game.round = 0;
   addCount();
 }
 
@@ -48,24 +23,13 @@ function newGame() {
   clearGame();
 }
 
-function strict() {
-  if (game.strict == false) {
-    game.strict = true;
-    $('#strict').html('Is currently On').removeClass('btn-primary').addClass('btn-danger');
-  } else {
-    game.strict = false;
-    $('#strict').html('Is currently Off').removeClass('btn-danger').addClass('btn-primary');
-  }
-  
-  newGame();
-}
 
 function showMoves() {
   var i = 0;
   var moves = setInterval(function(){
-    playGame(game.currentGame[i]);
+    playGame(game.sequence[i]);
     i++;
-    if (i >= game.currentGame.length) {
+    if (i >= game.sequence.length) {
       clearInterval(moves);
     }
   }, 600)
@@ -109,18 +73,18 @@ function gameClick(id) {
 } 
 
 function playerTurn(x) {
-  if (game.player[game.player.length - 1] !== game.currentGame[game.player.length - 1]) {
+  if (game.player[game.player.length - 1] !== game.sequence[game.player.length - 1]) {
     if(game.strict){
       newGame();
     } else {
-      alert('Sorry! Please try again!');
+      alert('Wrong move! Try again!');
       showMoves();
     }
    } else {
       sound(x);
-      var check = game.player.length === game.currentGame.length;
+      var check = game.player.length === game.sequence.length;
       if (check) {
-        if(game.count == 20){
+        if(game.round == 20){
           alert('You won! Congrats.');
         } else {
           nextLevel();
@@ -134,37 +98,20 @@ function nextLevel() {
 }
 
 function generateMove(){
-<<<<<<< HEAD
   game.sequence.push(game.colors[(Math.floor(Math.random()*4))]);
-=======
-  game.currentGame.push(game.possibilities[(Math.floor(Math.random()*4))]);
-  //alert(game.currentGame.length);
->>>>>>> c8768ec61d18a0e35bcdeceaf334129c9de8fce6
   showMoves();
 }
 
 function addCount() {
-<<<<<<< HEAD
   game.round++;
   $('#roundNumber').addClass('animated fadeOutDown');
   
   setTimeout(function(){
   
   $('#roundNumber').removeClass('fadeOutDown').html(game.round).addClass('fadeInDown');
-=======
-  game.count++;
-  $('#clickNumber').addClass('animated fadeOutDown');
-  
-  setTimeout(function(){
-    $('#clickNumber').removeClass('fadeOutDown').html(game.count).addClass('fadeInDown');
->>>>>>> c8768ec61d18a0e35bcdeceaf334129c9de8fce6
   }, 200);
   
   generateMove();
 }
 
 newGame();
-
-
-
-};
